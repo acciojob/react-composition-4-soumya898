119b5d4f6e60e762beb94b0639cb67129a87fa0b
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-09-19 02:45:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,57 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userList, setUserList] = useState([]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const newUser = { name, email, password };
+    setUserList([...userList, newUser]);
+    console.log(newUser);
+    // Clear input fields after submission
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
